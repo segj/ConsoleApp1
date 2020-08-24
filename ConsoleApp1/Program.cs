65ab649984d1d267e59aa7c8ed0928aba8733c31
Søren Engholm Gjesing <SGJ@kmd.dk>
2020-08-24 15:02:15 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using MCronberg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = MCronberg.PersonRepositoryStatic.JustGetPeople();
+            /* foreach (var person in people)
+                 Console.WriteLine(person.Gender);*/
+
+            var persons = people.Where(i => i.Height < 170 && i.IsHealthy).OrderBy(i => i.Name).ThenBy(i => i.Gender).Select(i => new { i.Name, i.Gender});
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
